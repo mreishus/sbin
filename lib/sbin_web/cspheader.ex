abb0b12de1049a0446ae2701aa5adb6933188a13
Matthew Reishus <mreishus@users.noreply.github.com>
2019-10-05 16:00:33 -0500

    Add content-security-policy header

---
+++ lib/sbin_web/cspheader.ex
@@ -0,0 +1,24 @@
+defmodule SbinWeb.CSPHeader do
+  @moduledoc """
+  Add a content-security-policy header to help prevent XSS attacks.
+  """
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    put_resp_header(conn, "content-security-policy", csp(conn))
+  end
+
+  defp csp(conn) do
+    "default-src 'self'; \
+    connect-src 'self' #{ws_url(conn)} #{ws_url(conn, "wss")}; \
+    script-src 'self' 'unsafe-inline' 'unsafe-eval'; \
+    style-src 'self' 'unsafe-inline' 'unsafe-eval'"
+  end
+
+  defp ws_url(conn, protocol \\ "ws") do
+    endpoint = Phoenix.Controller.endpoint_module(conn)
+    %{endpoint.struct_url | scheme: protocol} |> URI.to_string()
+  end
+end
--- lib/sbin_web/router.ex
+++ lib/sbin_web/router.ex
@@ -2,21 +2,22 @@ defmodule SbinWeb.Router do
   use SbinWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
+    plug(SbinWeb.CSPHeader)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", SbinWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
   end
 
   # Other scopes may use custom stacks.
