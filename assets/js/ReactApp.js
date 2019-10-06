bcbfc19bdfef15572a4ca56b3a30cc76a6be3e1f
Matthew Reishus <mreishus@users.noreply.github.com>
2019-10-06 11:52:20 -0500

    Get a simple mounting of react component working

---
+++ assets/js/ReactApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class ReactApp extends React.Component {
+  render() {
+    return (
+      <div>
+        This is a react app
+        <div className="alert alert-info">Testing</div>
+      </div>
+    );
+  }
+}
+export default ReactApp;
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import css from "../css/app.css";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,9 +9,20 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import "phoenix_html";
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactApp from "./ReactApp";
+
+let $ = f => document.querySelector(f);
+let $$ = f => document.getElementById(f);
+console.log("1");
+if ($$("react-app-root")) {
+  ReactDOM.render(<ReactApp />, $$("react-app-root"));
+}
