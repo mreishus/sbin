79307650bd0ccac728d2055e53823ec0d3fab7e7
Matthew Reishus <mreishus@users.noreply.github.com>
2019-10-14 02:04:04 +0000

    Remove some unused features I was playing around with

--- assets/js/app/AppRouter.tsx
+++ assets/js/app/AppRouter.tsx
@@ -3,16 +3,12 @@ import { Switch, Route } from "react-router-dom";
 
 import NoteIndex from "../pages/NoteIndex";
 import NoteShow from "../pages/NoteShow";
-import TestPage from "../pages/TestPage";
 
 interface Props {}
 
 export const AppRouter = (props: Props) => {
   return (
     <Switch>
-      <Route path="/test_page">
-        <TestPage />
-      </Route>
       <Route path="/note/:id/:password" component={NoteShow} />
       <Route path="/note" component={NoteIndex} />
       <Route path="/" component={NoteIndex} />
