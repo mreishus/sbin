a0e4dea02d770cb8cb8e10409d5a3dcc919ad395
Matthew Reishus <mreishus@users.noreply.github.com>
2019-10-16 10:05:20 -0500

    Add build.sh

--- assets/js/pages/AboutIndex.tsx
+++ assets/js/pages/AboutIndex.tsx
@@ -47,7 +47,7 @@ export const AboutIndex = () => {
           </a>
         </p>
         <h2 className="text-xl mt-2 text-green-300">version</h2>
-        <p className="mt-2">0.3.2</p>
+        <p className="mt-2">0.3.3</p>
       </div>
     </div>
   );
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This is used by the docker container to run migrations,
+# then start the server.
 
 ./bin/sbin eval "Sbin.Release.migrate"
 ./bin/sbin start
