7d476d524aafb6ef12a080bdf5ee4ca454250dcb
Matthew Reishus <mreishus@users.noreply.github.com>
2019-12-16 14:46:49 -0600

    1.0.5 release

--- assets/js/pages/AboutIndex.tsx
+++ assets/js/pages/AboutIndex.tsx
@@ -47,7 +47,7 @@ export const AboutIndex = () => {
           </a>
         </p>
         <h2 className="text-xl mt-2 text-green-300">version</h2>
-        <p className="mt-2">1.0.4</p>
+        <p className="mt-2">1.0.5</p>
       </div>
     </div>
   );
