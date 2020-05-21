2faf525de0bfb6bfecb0c6fba8a2731cdbf06ad4
Matthew Reishus <mreishus@users.noreply.github.com>
2020-05-21 08:53:05 -0500

    Add types to useDebounce

--- web/assets/js/hooks/useDebounce.ts
+++ web/assets/js/hooks/useDebounce.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 // Our hook
-export default function useDebounce(value, delay) {
+export default function useDebounce(value: any, delay: number) {
   // State and setters for debounced value
   const [debouncedValue, setDebouncedValue] = useState(value);
 
